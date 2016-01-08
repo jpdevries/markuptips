dfb75c40085f5b46d1fa36b1430d4d9b995be509
JP DeVries <johnpaul.devries@gmail.com>
2016-01-07 18:59:35 -0800

    Initial Commit

---
+++ _build/gruntfile.js
@@ -0,0 +1,357 @@
+module.exports = function(grunt) {
+    grunt.option.init({
+        'cssVer':(!grunt.option('cssVer')) ? grunt.file.readJSON('./config.json').cssVer : grunt.option('cssVer'),
+        'jsVer':(!grunt.option('jsVer')) ? grunt.file.readJSON('./config.json').jsVer : grunt.option('jsVer'),
+        'icons':(!grunt.option('icons')) ? grunt.file.readJSON('./config.json').icons : grunt.option('icons'),
+        'theme':(!grunt.option('theme')) ? grunt.file.readJSON('./config.json').theme : grunt.option('theme')
+    });
+
+    var doS3 = grunt.file.isFile('aws-secret.json');
+	// Project configuration.
+	var initConfig = {
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: { /* just defining some properties */
+			lib: './lib/',
+			scss: './scss/',
+			theme: grunt.option('theme'),
+			assets: 'assets/',
+			icons: 'icons/',
+			css: 'css/',
+			js:  'js/',
+			img: 'img/',
+			font: 'font/'
+		},
+		bower: {
+			install: {
+				options: {
+					targetDir: './lib',
+					layout: 'byComponent'
+				}
+			}
+		},
+		copy: {
+		},
+		cssmin: {
+			compress: {
+				options: {
+					report: 'min',
+					keepSpecialComments: 0,
+					banner: '/*!\n*  <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n*/'
+				},
+				files: {
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.min.css': '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>opendyslexic.min.css': '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>opendyslexic.css'
+				}
+			}
+		},
+        webfont: {
+            icons: {
+                src: './icons/*.svg',
+                dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.font %>',
+                destCss:'<%= dirs.scss %>base/',
+                options: {
+                    font:'modmoreicons',
+                    hashes:true,
+                    engine:'node',
+                    destHtml:'<%= dirs.theme %><%= dirs.assets %><%= dirs.font %>',
+                    relativeFontPath: '../<%= dirs.font %>',
+                    //template:'<%= dirs.icons %>bem.css',
+                    htmlDemo:true,
+                    htmlDemoTemplate:'<%= dirs.icons %>demo.html',
+                    templateOptions:{
+                        baseClass: "mm",
+                        classPrefix: "mm-",
+                        mixinPrefix: "mm_",
+                        stylesheet:'scss'
+                    }
+                }
+            }
+        },
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					compass: false
+				},
+				files: {
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css': '<%= dirs.scss %>main.scss',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>opendyslexic.css': '<%= dirs.scss %>base/opendyslexic.scss'
+				}
+			},
+			dev: {
+				options: {
+					style: 'expanded',
+					compass: false
+				},
+				files: {
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css': '<%= dirs.scss %>main.scss',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>opendyslexic.css': '<%= dirs.scss %>base/opendyslexic.scss'
+				}
+			}
+		},
+
+		csslint: {
+			strict: {
+				options: {
+					import: 2
+				},
+				src: ['<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>**/*.css']
+			}
+		},
+
+		concat: {
+			options: {
+				separator: '',
+			}
+
+			,
+			js: {
+				src: [
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/mustache.js',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/typeahead.js/typeahead.bundle.js',
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>plugins.js',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/jquery.speakable.js',
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main.js',
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>drivers/react/react.js'
+				],
+				dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main-dev.js',
+			}
+
+		},
+		uglify: {
+			main: {
+				options: {
+					report: 'min'
+				},
+				files: {
+					'<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main-min.js': ['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main-dev.js'],
+                    '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>drivers/react/react.min.js': ['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>drivers/react/react.js']
+				}
+
+			}
+		},
+        compress: {
+          css: {
+            options: {
+              mode: 'gzip'
+            },
+            src: ['<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.min.css'],
+            dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.min.' + (grunt.option('cssVer')) + '.cssgz'
+          },
+          js: {
+            options: {
+              mode: 'gzip'
+            },
+            src: ['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main-min.js'],
+            dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>main-min.' + (grunt.option('jsVer')) + '.jsgz'
+          }
+        },
+		watch: { /* trigger tasks on save */
+			options: {
+				livereload: true
+			},
+
+			scss: {
+				files: '<%= dirs.scss %>**/*.scss',
+				tasks: ['sass:dev', 'growl:sass']
+			},
+
+			js: {
+				files: ['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>*','!<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>**/*-dev.js*','!<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>**/*-min.js*'],
+				tasks: ['concat', 'growl:concat', 'uglify', 'growl:uglify']
+			}
+		},
+		clean: { /* take out the trash */
+			options: {
+				force: true
+			},
+			prebuild: ['<%= dirs.scss %>bourbon', '<%= dirs.scss %>font-awesome'],
+			postbuild: ['<%= dirs.lib %>']
+		},
+        shell: {
+            jsx: {
+                command:'jsx js/drivers/react/ <%= dirs.theme %><%= dirs.assets %><%= dirs.js %>drivers/react/',
+                options: {
+                    execOptions:{
+                        cwd:'./'
+                    }
+                }
+            }
+        },
+		growl: { /* optional growl notifications requires terminal-notifer: gem install terminal-notifier */
+
+			sass: {
+				message: "Sass files created.",
+				title: "grunt"
+			},
+
+			build: {
+				title: "grunt",
+				message: "Build complete."
+			},
+			watch: {
+				title: "grunt",
+				message: "Watching. Grunt has its eye on you."
+			},
+			expand: {
+				title: "grunt",
+				message: "CSS Expanded. Don't check it in."
+			},
+			concat: {
+				title: "grunt",
+				message: "JavaScript concatenated."
+			},
+			uglify: {
+				title: "grunt",
+				message: "JavaScript minified."
+			}
+		}
+	};
+
+
+	initConfig.copy["bourbon"] = {
+		files: [{
+			src: 'bourbon/**/*',
+			cwd: '<%= dirs.lib %>',
+			dest: '<%= dirs.scss %>',
+			expand: true
+		}]
+	};
+
+	initConfig.copy["neat"] = {
+		files: [{
+			src: 'neat/**/*',
+			cwd: '<%= dirs.lib %>',
+			dest: '<%= dirs.scss %>',
+			expand: true
+		}]
+	};
+
+	initConfig.copy["typeahead"] = {
+		files: [{
+			src: 'typeahead.js/**/*',
+			cwd: '<%= dirs.lib %>',
+			dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/',
+			expand: true
+		}]
+	};
+
+	initConfig.copy["mustache"] = {
+		files: [{
+			src: './**/*',
+			cwd: '<%= dirs.lib %>mustache.js/',
+			dest: '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/',
+			expand: true
+		}]
+	};
+
+    if(grunt.file.isFile('./config.json')) {
+        initConfig.bump = {
+            pkg:{
+                files: [
+                    {src:'config.json',dest:'config.json'}
+                ],
+                options: {
+                    replacements:[{
+						pattern:/"jsVer":\s*"\d+.\d+.\d+"/ig,
+						replacement:'"jsVer": "' + grunt.option('jsVer') + '"'
+                    },{
+						pattern:/"cssVer":\s*"\d+.\d+.\d+"/ig,
+						replacement:'"cssVer": "' + grunt.option('cssVer') + '"'
+                    }]
+                }
+            }
+        }
+    }
+
+    if(doS3) {
+        initConfig.aws = grunt.file.readJSON('aws-secret.json');
+        initConfig.s3 = {
+            options: {
+                accessKeyId: "<%= aws.accessKeyId %>",
+                secretAccessKey: "<%= aws.secretAccessKey %>",
+                bucket: "<%= aws.bucket %>"
+            },
+            cssgz: {
+                cwd:'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>',
+                src:'main.min.' + (grunt.option('cssVer')) + '.cssgz',
+                dest:'<%= dirs.assets %><%= dirs.css %>',
+                options:{
+                    gzip:false,
+                    headers:{
+                        ContentType:'text/css',
+                        CacheControl:31556926,
+                        Expires: new Date(new Date().setYear(new Date().getFullYear() + 1)),
+                        ContentEncoding:'gzip'
+                    }
+                }
+            },
+            jsgz: {
+                cwd:'<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>',
+                src:'main-min.' + (grunt.option('jsVer')) + '.jsgz',
+                dest:'<%= dirs.assets %><%= dirs.js %>',
+                options:{
+                    gzip:false,
+                    headers:{
+                        ContentType:'application/javascript',
+                        CacheControl:31556926,
+                        Expires: new Date(new Date().setYear(new Date().getFullYear() + 1)),
+                        ContentEncoding:'gzip'
+                    }
+                }
+            },
+            sf:{
+                cwd: "<%= dirs.theme %><%= dirs.assets %>font/sf",
+                src: "**",
+                dest: "<%= dirs.assets %>font/sf/",
+                options:{
+                    headers:{
+                        CacheControl:31556926,
+                        Expires: new Date(new Date().setYear(new Date().getFullYear() + 1))
+                    }
+                }
+            },
+            fira:{
+                cwd: "<%= dirs.theme %><%= dirs.assets %>font/fira",
+                src: "**",
+                dest: "<%= dirs.assets %>font/fira/",
+                options:{
+                    headers:{
+                        CacheControl:31556926,
+                        Expires: new Date(new Date().setYear(new Date().getFullYear() + 1))
+                    }
+                }
+            }
+        }
+    }
+
+	grunt.initConfig(initConfig);
+
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-growl');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.renameTask('string-replace','bump');
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-aws');
+    grunt.loadNpmTasks('grunt-shell');
+
+
+	grunt.registerTask('default', ['growl:watch', 'watch']);
+	grunt.registerTask('build', ['clean:prebuild', 'bower', 'copy', 'sass:dev', 'cssmin', 'concat', 'uglify', 'growl:sass', 'clean:postbuild']);
+	grunt.registerTask('expand', ['sass:dev', 'growl:sass', 'growl:expand']);
+    grunt.registerTask('s3gz',['compress','s3:cssgz','s3:jsgz'])
+    grunt.registerTask('jsx',['shell:jsx'])
+};
---
+++ _build/js/drivers/react/react.js
@@ -0,0 +1,176 @@
+var GridItem = React.createClass({
+  handleClick:function(){
+      if(!this.props.data.is_container) return;
+      this.props.handleClick($('#resource-view-reactable').data('fetch') + this.props.data.id);
+  },
+  render: function() {
+    var icon;
+    if(this.props.data.is_container) {
+        icon = (this.props.data.conatiner_open) ? 'fa fa-folder-open' : 'fa fa-folder';
+    } else {
+        switch(this.props.data.class_key) {
+          case 'modWebLink':
+              icon = 'fa fa-link';
+          break;
+      
+          default:
+              icon = (this.props.data.linkattributes) ? ('fa ' + 'fa-' + (this.props.data.linkattributes)) : 'fa fa-file-text-o'
+          break;
+        }        
+    }
+    
+    //var titleAttr = "ID: " + this.props.data.id + "; " + "Published";
+    var cur = (this.props.data.id == MODx.resource.id) ? true : false;
+    var link = (cur) ? <a href={this.props.data.href}><strong>{decodeHTML(this.props.data.title)}</strong></a> : <a href={this.props.data.href}>{decodeHTML(this.props.data.title)}</a>;
+    var publishedOn = (this.props.data.publishedon) ? new Date(this.props.data.publishedon).toLocaleDateString() : ' ';
+    return (
+        <tr>
+            <td> 
+              <i onClick={this.handleClick} className={icon}></i>&nbsp;
+              {link}
+            </td>
+            <td>{decodeHTML(this.props.data.introtext)}</td>
+            <td>{publishedOn}</td>
+        </tr>
+      );
+  }
+});
+
+var Grid = React.createClass({
+    getInitialState: function() {
+        return {
+            items: [],
+            path:[''],
+            resource:{
+                id:"",
+                alias:""
+            }
+        };
+    },
+    handleMount: function() {
+        $.ajax({
+          url: this.props.url,
+          dataType: 'json',
+          success: function(data) {
+            this.setState(data);
+            //if(this.props.stateSetCallback) this.props["stateSetCallback"]();
+            //window.dispatchEvent(new Event('resource_view__stateSet')); //IE 11 dies here
+          }.bind(this),
+          error: function(xhr, status, err) {
+              //console.log('error');
+          }.bind(this)
+        });
+        
+        /*
+        var previewClicked = function(e){
+            e.preventDefault();
+            $('body').toggleClass('inverted');    
+        };
+        */
+        //$('a.preview').unbind('click',previewClicked).bind('click',previewClicked);
+    },
+    componentDidMount: function() {
+        this.handleMount();
+    },
+    handleClick:function(fetch){
+        this.props.url = fetch;
+        this.handleMount();
+    },
+    handleSlugClick:function(fetch){
+        this.handleClick(fetch);
+    },
+    render: function() {
+        var that = this;
+        var context = 'web';
+        return (
+            <div>
+              <h4>
+                <i className="fa fa-code"></i>&nbsp;<code>markup.tips</code>
+                <div className="codepath">
+                    <pre title="Imagine this is interactive, terminal like with tab-to-complete">{this.state.path.map(function(item, i){
+                        return (
+                            <span>
+                              <a data-fetch={item.fetch} className="clickable" onClick={that.handleSlugClick.bind(this, item.fetch)}>{item.slug}</a>
+                              <span>/</span>
+                            </span>
+                        );
+                    })}</pre>
+                </div>
+              </h4>
+              <table border="0" cellspacing="0" cellpadding="0">
+              <thead>
+                  <tr>
+                    <th>Page</th>
+                    <th>Description</th>
+                    <th>Published On</th>
+                  </tr>
+              </thead>
+              <tbody id="resource-view-body">
+                {this.state.items.map(function (item, i) {
+                    return (
+                        <GridItem handleClick={that.handleClick} data={item} key={context + item.id} ref={'item'+i} ></GridItem>
+                    );
+                })}
+              </tbody>        
+              </table>
+            </div>
+        );
+    }
+});
+
+var b = '0';
+try { b = MODx.tree.branch; } catch (e) {} 
+var j = $('#resource-view-reactable').data('fetch') + b;
+React.render(<Grid url={j} />, document.getElementById('resource-view-reactable'));
+
+var TodoList = React.createClass({
+  itemClicked: function(i) {
+      //console.log(event);
+      this.props.handleItemClicked(i);
+  },
+  render: function() {
+  var that = this;
+    var createItem = function(item,i) {
+        var icon = 'fa ' + ((item.done) ? 'fa-check-circle-o' : 'fa-circle-o');
+        return <li onClick={that.itemClicked.bind(that,i)}><p><i className={icon}></i>&nbsp;{item.text}</p></li>;
+    };
+    return <ul className="naked">{this.props.items.map(createItem)}</ul>;
+  }
+});
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {text:'', items: [ // pretend this comes from server
+        {id:0,text:<span>Follow <a href="http://twitter.com/mrktps" target="_blank" onClick={function(event){event.stopPropagation();}}>@mrktps</a></span>,done:false},
+        {id:2,text:<span>Learn how this Component <a href="components/accessible-todo.php" onClick={function(event){event.stopPropagation();}}>was built</a></span>,done:false}
+    ]};
+  },
+  addItem: function(e) {
+      this.setState({items: this.state.items.concat({text:this.state.text,done:false}), text:''});
+      this.refs.TaskTitle.getDOMNode().focus();
+      return false;
+  },
+  handleItemClicked: function(i) {
+      this.state.items[i].done = !this.state.items[i].done;
+      this.setState({items: this.state.items});
+  },
+  onChange: function(e) {
+      this.setState({text: e.target.value});
+  },
+  render: function() {
+      var _disabled = this.state.text == '' ? true : false;
+    return (
+        <form className="box" onSubmit={this.addItem}>
+            <h2 id="task-list"><i className="fa fa-circle-o"></i><br />Task List</h2>
+            <TodoList items={this.state.items} handleItemClicked={this.handleItemClicked} />
+            <footer className="add-item">
+                <input required ref="TaskTitle" type="text" name="text" onChange={this.onChange} value={this.state.text} placeholder="Add a new item&hellip;" />
+                <button disabled={_disabled} type="submit">
+                    <i className="fa fa-plus"></i>
+                </button>
+            </footer>
+        </form>
+    );
+  }
+});
+
+React.render(<TodoApp />, document.getElementById('task-list'));
